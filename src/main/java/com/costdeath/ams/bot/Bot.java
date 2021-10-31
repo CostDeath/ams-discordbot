f4e1c3fe8900d32178e903e85d437faa13f3da1a
CostDeath <cost.gamer123@gmail.com>
2021-10-31 11:50:19 +0000

    Changed data storing to .properties instead of .txt

--- src/main/java/com/costdeath/ams/bot/Bot.java
+++ src/main/java/com/costdeath/ams/bot/Bot.java
@@ -2,31 +2,20 @@ package com.costdeath.ams.bot;
 
 import net.dv8tion.jda.api.*;
 
-import java.io.File;
-import java.util.Scanner;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.util.Properties;
 
 public class Bot {
-    public String token;
-    public String prefix;
-
-    public void assignInfo(String[] line) {
-        switch(line[0]) {
-            case "token": this.token = line[1];
-            case "prefix": this.prefix = line[1];
-        }
-    }
 
     public static void main(String[] arguments) throws Exception {
-        Bot bot = new Bot();
-        File botInfoFile = new File(System.getProperty("user.dir") + "\\botinfo.txt");
-        Scanner reader = new Scanner(botInfoFile);
-        while(reader.hasNextLine()) {
-            String[] line = reader.nextLine().split("=");
-            bot.assignInfo(line);
-        }
+        InputStream botInfoFile = new FileInputStream(System.getProperty("user.dir") + "\\botinfo.properties");
+        Properties bot = new Properties();
+        bot.load(botInfoFile);
+        System.out.println();
 
-        JDA api = JDABuilder.createDefault(bot.token)
-                .addEventListeners(new Commands(bot.prefix))
+        JDA api = JDABuilder.createDefault(bot.getProperty("token"))
+                .addEventListeners(new Commands(bot.getProperty("prefix")))
                 .build();
     }
 }
